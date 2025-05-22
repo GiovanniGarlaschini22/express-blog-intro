f2489c7dbf193e508ac5423b68639aad2fdad517
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-05-22 16:19:07 +0200

    Feat: reso accessibile la cartella immagini

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 // creo la porta di accesso //
 const port = 3000;
 
+// rendo accessibile la cartella immagini //
+app.use(express.static("public"));
+
 // metto il server in ascolto //
 app.listen(port, () => {
     console.log("server in ascolto");  
